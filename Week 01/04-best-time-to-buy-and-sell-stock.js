1d5c94e95b8f7a5dea9620ddbe0426a5ddca1e29
Ryan Nerby <ryannerby@Ryans-MacBook-Air.local>
2025-10-24 10:29:09 -0400

    Complete best time to buy and sell stock solution
    - Implemented O(n) solution using single pass approach
    - Added commented O(n^2) brute force solution for reference
    - Fixed loop bounds to include all array elements
    - Added comprehensive test cases

--- Week 01/04-best-time-to-buy-and-sell-stock.js
+++ Week 01/04-best-time-to-buy-and-sell-stock.js
@@ -27,6 +27,36 @@
  */
 var maxProfit = function(prices) {
     // Your solution here
+    /* O(n^2)
+    let mostProfit = 0;
+ 
+    for (let i = 0; i < prices.length - 1; i++) {
+        let buy = prices[i];
+        for (let j = i + 1; j < prices.length; j++) {
+            let sell = prices[j];
+            if (sell - buy > mostProfit) {
+                mostProfit = sell - buy;
+            }
+        }
+    }
+
+    return mostProfit;
+    */
+
+    let minPrice = prices[0]
+    let maxProfit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+
+
 };
 
 // Test cases
